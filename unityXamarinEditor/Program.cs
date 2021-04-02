22603a1d29363ec91218a96766af10c7096ff3e7
kvnxp <kvnxp@hotmail.com>
2021-04-01 22:47:05 -0500

    use shell

--- unityXamarinEditor/Program.cs
+++ unityXamarinEditor/Program.cs
@@ -11,28 +11,44 @@ namespace args
 
 		public static void Main(string[] args)
 		{
+
+//			Process.EnterDebugMode();
 			string appname = "XamarinStudio.exe";
 			Process[] pro = Process.GetProcessesByName(appname.Replace(".exe", ""));
+			
 			string[] fullcommand = Environment.GetCommandLineArgs();
 
-			if (pro.Length == 0)
+			if (fullcommand.Length > 2)
 			{
-
-				FileInfo finfo = new FileInfo(fullcommand[0]);
-				string arguments = fullcommand[2] + " " + fullcommand[3].Replace("-1", "0");
-				Process.Start(finfo.Directory + "/" + appname, arguments);
+					FileInfo finfo = new FileInfo(fullcommand[0]);
+				
+				if (pro.Length == 0)
+				{
+					string arguments = fullcommand[2] + " " + fullcommand[3].Replace("-1", "0");
+//					Process.Start(finfo.Directory + "/" + appname, arguments);
+					runprocess(finfo.Directory + "/" + appname ,arguments);
+				}
+				else
+				{
+					Regex reg = new Regex(".+cs");
+					Match command = reg.Match(fullcommand[3]);
+					runprocess(finfo.Directory + "/" + appname ,command.Value);
+				}
+				//Console.ReadKey();
 			}
-			else
-			{
-				Regex reg = new Regex(".+cs");
-				Match command = reg.Match(fullcommand[3]);
-
-				Process.Start(command.Value);
-			}
-			//Console.ReadKey();
-
 		}
-
+		
+		public static void runprocess(string process,string arguments){
+			ProcessStartInfo info = new ProcessStartInfo();
+			info.FileName = process;
+			info.Arguments = arguments;
+			info.UseShellExecute = true;
+			Process ps = new Process();
+			ps.StartInfo = info;
+			ps.Start();
+		
+		
+		}
 
 
 	}
